86727bc16f02e08e415b6af8a8a4a0e8c1cffd1e
Matthew Maday <junkmail@oncalinteractive.com>
2013-07-03 16:35:21 -0500

    Fixed multi-orientation issue
    Ready for FB integration

--- application/views/card.lua
+++ application/views/card.lua
@@ -181,18 +181,18 @@ function LoadCard:new(params)
 			-- banner text
 			tweenObject(pBannerTxt,pBannerTxt.x,0, -225, -155,1, 1)
 
-		else
+		elseif system.orientation == "landscapeRight" or system.orientation == "landscapeLeft" then
 
 			-- card
 			pCard.xScale,pCard.yScale = .75,.75
-			tweenObject(screen, screen.x,self.centerY*.5, screen.y, self.centerX,.5, 1)
+			tweenObject(screen, screen.x,self.centerY, screen.y, self.centerX,.5, 1)
 			
 			-- banner
 			tweenObject(pBanner, pBanner.x,0, -220, -150,1, 1)
-
+			
 			-- banner text
 			tweenObject(pBannerTxt,pBannerTxt.x,0, -225, -155,1, 1)
-
+			
 		end
 
 	end
